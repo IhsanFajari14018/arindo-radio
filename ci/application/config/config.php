ebbb6f6f2d62bf0cf52c60e29c1b47cb30dad3c6
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-04-21 15:30:31 +0700

    Update config.php

--- ci/application/config/config.php
+++ ci/application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+// $config['base_url'] = 'http://localhost/arindo-radio/ci/';
+$config['base_url'] = 'http://rs-ap.id/ci/';
+
 
 /*
 |--------------------------------------------------------------------------
